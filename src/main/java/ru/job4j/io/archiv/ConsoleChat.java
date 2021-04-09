7f0676d0fbbbdb6352f551f376bea0fa5f539783
MikhailPushkarev25 <mixa199525@yandex.ru>
2021-04-09 23:04:14 +0300

    6. Кодировка. [#297026]

--- src/main/java/ru/job4j/io/archiv/ConsoleChat.java
+++ src/main/java/ru/job4j/io/archiv/ConsoleChat.java
@@ -5,8 +5,6 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -31,32 +29,6 @@ public class ConsoleChat {
         this.botAnswers = botAnswers;
     }
 
-    public void run() {
-        boolean flag = true;
-        Scanner scanner = new Scanner(System.in);
-        String res = scanner.nextLine();
-        writeFile(botAnswers, "Bot: " + res);
-        while (!res.equals(OUT)) {
-            if (res.equals(STOP)) {
-                System.out.println("Stop bot");
-                flag = false;
-            }
-            if (res.equals(CONTINUE)) {
-                System.out.println("Continue bot");
-                flag = true;
-            }
-            if (flag) {
-                String answer = readFile(path);
-                System.out.println(answer);
-                writeFile(botAnswers, "Bot: " + answer);
-            }
-            res = scanner.nextLine();
-            writeFile(botAnswers, "Usage Massage" + res);
-        }
-        scanner.close();
-
-    }
-
     public String readFile(String path) {
         StringBuilder builder = new StringBuilder();
         try (BufferedReader br = new BufferedReader(new FileReader(path, Charset.forName("UTF-8")))) {
@@ -64,14 +36,13 @@ public class ConsoleChat {
             while ((data = br.read()) > 0) {
                 builder.append((char) data);
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
-        String[] result = builder.toString().split(" ");
+        String[] str = builder.toString().split(" ");
         Random random = new Random();
-        int index = random.nextInt(result.length);
-        return result[index];
+        int index = random.nextInt(str.length);
+        return str[index];
     }
 
     public void writeFile(String path, String data) {
@@ -82,8 +53,33 @@ public class ConsoleChat {
         }
     }
 
+    public void run() {
+        boolean flag = true;
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        writeFile(botAnswers, "User: " + str);
+        while (!str.equals(OUT)) {
+            if (str.equals(STOP)) {
+                System.out.println("Bot STOP: ");
+                flag = false;
+            }
+            if (str.equals(CONTINUE)) {
+                System.out.println("Bot CONTINUE: ");
+                flag = true;
+            }
+            if (flag) {
+                String answer = readFile(path);
+                System.out.println(answer);
+                writeFile(botAnswers, "Bot: " + answer);
+            }
+            str = scanner.nextLine();
+            writeFile(botAnswers, "User: " + str);
+        }
+        scanner.close();
+    }
+
     public static void main(String[] args) {
-        ConsoleChat cc = new ConsoleChat("./src/main/console.txt", "./src/main/console_log.txt");
+        ConsoleChat cc = new ConsoleChat("./src/main/bot.txt", "./src/main/bot_log.txt");
         cc.run();
     }
 }
