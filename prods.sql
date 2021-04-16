df6150bb37c57236e1a816f28ce4b9c01516c778
MikhailPushkarev25 <mixa199525@yandex.ru>
2021-04-16 21:21:35 +0300

    Фильтры [#296953]

--- prods.sql
+++ prods.sql
@@ -11,39 +11,41 @@ create table product(
 	price float
 );
 
-insert into type(name) values('Сыр');
-insert into type(name) values('Хлеб');
-insert into type(name) values('Зелень');
-insert into type(name) values('Мороженое');
+insert into type(name) values('РЎС‹СЂ');
+insert into type(name) values('РҐР»РµР±');
+insert into type(name) values('Р—РµР»РµРЅСЊ');
+insert into type(name) values('РњРѕСЂРѕР¶РµРЅРѕРµ');
 
-insert into product(name, type_id, expired_date, price) values ('Сыр Ламберд', 4, date '14.05.2021', 1);
-insert into product(name, type_id, expired_date, price) values ('Сыр Президент', 6, date '13.05.2021', 1);
+insert into product(name, type_id, expired_date, price) values ('РЎС‹СЂ Р›Р°РјР±РµСЂРґ', 4, date '14.05.2021', 1);
+insert into product(name, type_id, expired_date, price) values ('РЎС‹СЂ РџСЂРµР·РёРґРµРЅС‚', 6, date '13.05.2021', 1);
 
-insert into product(name, type_id, expired_date, price) values ('Хлеб Белый', 20, date '20.04.2021', 2);
-insert into product(name, type_id, expired_date, price) values ('Хлеб Черный', 4, date '25.04.2021', 2);
+insert into product(name, type_id, expired_date, price) values ('РҐР»РµР± Р‘РµР»С‹Р№', 20, date '20.04.2021', 2);
+insert into product(name, type_id, expired_date, price) values ('РҐР»РµР± Р§РµСЂРЅС‹Р№', 4, date '25.04.2021', 2);
 
-insert into product(name, type_id, expired_date, price) values ('Зелень Лук', 7, date '14.05.2021', 3);
-insert into product(name, type_id, expired_date, price) values ('Зелень Укроп', 9, date '13.05.2021', 3);
+insert into product(name, type_id, expired_date, price) values ('Р—РµР»РµРЅСЊ Р›СѓРє', 7, date '14.05.2021', 3);
+insert into product(name, type_id, expired_date, price) values ('Р—РµР»РµРЅСЊ РЈРєСЂРѕРї', 9, date '13.05.2021', 3);
 
-insert into product(name, type_id, expired_date, price) values ('Мороженое стаканчик', 30, date '17.04.2021', 4);
-insert into product(name, type_id, expired_date, price) values ('Мороженое пломбир', 1, date '17.04.2021', 4);
+insert into product(name, type_id, expired_date, price) values ('РњРѕСЂРѕР¶РµРЅРѕРµ СЃС‚Р°РєР°РЅС‡РёРє', 30, date '17.04.2021', 4);
+insert into product(name, type_id, expired_date, price) values ('РњРѕСЂРѕР¶РµРЅРѕРµ РїР»РѕРјР±РёСЂ', 1, date '17.04.2021', 4);
 
-select name from product where price = 1;
+select * from product p join type t on t.id = p.type_id	 where t.name = 'РЎС‹СЂ';
 
-select * from product where name like 'Мороженое%';
+select * from product where name like 'РњРѕСЂРѕР¶РµРЅРѕРµ%';
 
-select current_date < current_date + interval '1 month'; 
+select * from product where date_part('month', expired_date) = date_part('month', current_date + interval '1 month');
 
-select * from product where price > 3;
+select * from product where price = (select max(price) from product);
 
-select name, type_id from product;
+select t.name, count(t.id) from type t join product p on t.id = p.type_id group by t.name;
 
-select * from product where (name like 'С%' or name like 'М%');
+select * from product p join type t on t.id = p.type_id where t.name in ('РЎС‹СЂ', 'РњРѕР»РѕРєРѕ');
 
-select * from product where type_id < 10;
+select t.name, count(t.id) from type t join product p on t.id = p.type_id group by t.name having count(t.id) < 10;
 
 select * from type;
 
-select * from product;
+select * from product p join type t where t.id = p.type_id;
+
+
 
 
